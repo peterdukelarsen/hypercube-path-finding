6b45537d7ec42b7b8a09985fb66a1a284d4ec2c0
pranavmayuram <pranav.mayuram@gmail.com>
2015-12-14 00:51:11 -0500

    Update website with working path

--- hypercube.js
+++ hypercube.js
@@ -1,7 +1,7 @@
 var hypercube = angular.module('hypercube', ["ngSanitize"]);
 
 hypercube.controller("hypercube_control", function($scope) {
-	// $scope.printing = false;
+	$scope.printing = false;
 	// $scope.dim_in = 3;
 	// $scope.indexA = 0;
 	// $scope.indexB = 3;
@@ -42,20 +42,34 @@ var global_dim;
 var makePath = function(vertices, a_array, b_array) {
 	var printString = "";
 	var ham = Hamming(a_array, b_array);
+	console.log("WOOT NOT FUCKED RD 2");
 	var path = []; // path is an array of vertices (array of arrays)
-	var temp_vert = a_array;
-	path.push(temp_vert);
+	var temp_vert = [];
+	path[0] = a_array;
+	for (l=0; l < a_array.length; ++l) {
+		temp_vert[l] = a_array[l];
+	}
+	console.log("path[0]:");
+	console.log(path[0]);
+	console.log("a_array:");
+	console.log(a_array);
 	var j = 0;
 	for (var i = 0; i < ham; ++i) {
-		if (temp_vert[j] == b_array[j]) {
+		if (temp_vert[j] === b_array[j]) {
 			--i;
 		}
 		else {
 			temp_vert[j] = b_array[j];
-			path.push(temp_vert);
+			var tempeh = [];
+			console.log("temp_vert.length: " + temp_vert.length);
+			for (z=0; z < temp_vert.length; ++z) {
+				tempeh[z] = temp_vert[z];
+			}
+			path.push(tempeh);
 		}
 		++j;
 	}
+	console.log(path);
 	var temp = -1;
 	printString += "Possible path: <br/>";
 	for (g = 0; g < path.length; ++g) {
@@ -86,6 +100,7 @@ var spaced = function(someString, size) {
 	for (var i = startsize; i < size; ++i) {
 		space_string += " ";
 	}
+	console.log(someString + space_string);
 	return (someString + space_string);
 }
 
@@ -97,8 +112,8 @@ var printVertices = function(vertices, printI) {
 			printString += (spaced(i.toString(), 3) + " ");
 		}
 		printString += "<br/>";
-		for (var j=0; j < vertices.length; ++j) {
-			printString += spaced("- ", 4);
+		for (var j=0; j < temp; ++j) {
+			printString += spaced("-", 4);
 		}
 		printString += "<br/>";
 	}
@@ -121,18 +136,35 @@ var main1 = function(dim_in) {
 	var vertices = [];
 	var indexA, indexB, ham;
 	var printString = "";
-	printString += "Input Dimensions:";
+	printString += ("Input Dimensions: " + dim_in);
 	dim = dim_in;
 	if (tempNode.length > dim_in) {
 		tempNode.slice(0, dim_in);
 	}
-	printString += "<br/>-------------------------------------------------------<br/>";
-	for (i=0; i < Math.pow(2,dim); ++i) {
-		for (j=0; j < dim; ++j) {
-			tempNode[j] = ((i) & (Math.pow(2,j)));
+	printString += "<br/><br/>";
+	for (var i=0; i < Math.pow(2,dim); ++i) {
+		var temp = 0x0001;
+		for (var j=0; j < dim; ++j) {
+			//if(((i & 0x0ffff)) == (((i & 0x0ffff) & ((0x0ffff & Math.pow(2,j))>>> 0) >>> 0)))
+			//if((((0x1 <<< j) >>> 1)))/* & (i >>> 0))>>>0*/
+			if(((0x1 << j) & (i >>> 0) )>>> 0)
+			{
+				tempNode[j] = 1;
+			}
+			else
+			{
+				tempNode[j] = 0;
+			}
+			//tempNode[j] = (i && ((i) & (Math.pow(2,j))));
+			//tempNode[j] = ((i==0) && (Math.pow(2, j) == 0));
 		}
-		vertices.push(tempNode);
+		var temp2 = [];
+		for (p = 0; p < tempNode.length; ++p) {
+			temp2.push(tempNode[p]);
+		}
+		vertices.push(temp2);
 	}
+	tempNode.push(0);
 	printString += "Vertices:<br/>";
 	if(dim >= 6) {
 		printString += (Math.pow(2, dim) + " Too many vertices to print<br/>Still possible to find shortest paths between vertices on the hypercube");
@@ -147,11 +179,15 @@ var main1 = function(dim_in) {
 
 var main2 = function(dim, vertices, indexA, indexB) {
 	var printString = "";
+	console.log(vertices[indexA]);
+	console.log(vertices[indexB]);
 	var ham = Hamming(vertices[indexA], vertices[indexB]);
+	console.log("WOOT WE NOT COMPLETELY FUCKED");
 	printString += "<br/>-------------------------------------------------------<br/>";
 	printString += "The Hamming distance (H(A, B)) is: " + ham + "<br/>";
 	printString += "Parallel paths of any length less than: " + dim + "<br/>";
 	printString += "Parallel paths, length H(A, B) : " + ham + "<br/><br/>";
+	console.log("making path");
 	printString += makePath(vertices, vertices[indexA], vertices[indexB]);
 	return printString;
 	// document.getElementById(element_Path).innerHTML = printString;
